0659f7cb2f045c8d46e85bde2d6904c9cb9b64d3
Grapensparr <113124538+Grapensparr@users.noreply.github.com>
2023-02-09 12:16:07 +0100

    API + error tests added

--- cypress/e2e/movieApp.cy.ts
+++ cypress/e2e/movieApp.cy.ts
@@ -12,34 +12,52 @@ describe('Should test page setup', () => {
     });
 });
 
-describe('Should test the display of mock movies', () => {
-    it('Should display list of mock movies', () => {
-        cy.intercept('GET', 'http://omdbapi.com/?apikey=416ed51a&s=*', {fixture: 'mockListOfMovies'}).as('MockCall');
-        cy.get('#searchText').type('Remember Me').should('have.value', 'Remember Me');
+describe('Should test the retrieval of data from the API', () => {
+    it("should display list of results from the actual API", () => {
+        cy.get('#searchText').type('Avatar').should('have.value', 'Avatar');
         cy.get('form').submit();
-        
-        cy.get('#movie-container > div').should('have.length', 5);
+    
         cy.get('#movie-container > div').should('have.class', 'movie');
-        cy.get('.movie > h3').should('have.length', 5);
-        cy.get('.movie > img').should('have.length', 5);
-        cy.get('h3').should('contain', 'Remember Me');
-        cy.get('h3').should('contain', 'The Green Mile');
-        cy.get('h3').should('contain', 'The Departed');
-        cy.get('h3').should('contain', 'Anaconda');
-        cy.get('h3').should('contain', 'Shutter Island');
+        cy.get('#movie-container > div').should('have.length', 10);
+        cy.get('.movie > img').should('have.length', 10);
+        cy.get('.movie > h3').should('have.length', 10);
+        cy.get('.movie > h3').should('contain', 'Avatar');
     });
-});
 
-describe('Should test error message', () => {
     it('Should display an error message, clear field', () => {
-        cy.get('#searchText').clear();
+        cy.get('#searchText').should('have.value', '');
         cy.get('form').submit();
         cy.get('p').contains('Inga sökresultat att visa');
     });
 
-    it('Should display an error message, no results', () => {
+    it('Should display an error message, no results found', () => {
         cy.get('#searchText').type('asdfgh').should("have.value", 'asdfgh');
         cy.get('form').submit();
         cy.get('p').contains('Inga sökresultat att visa');
+    });
+
+    it('Should display an error message, unable to fetch data', () => {
+        cy.intercept('GET', 'http://omdbapi.com/?apikey=416ed51a&s=*', { success: [] });
+        cy.get('#searchText').type('Avatar').should("have.value", 'Avatar');
+        cy.get('form').submit();
+        cy.get('p').contains('Inga sökresultat att visa');
+    });
+});
+
+describe('Should test the display of mock movies', () => {
+    it('Should display list of mock movies', () => {
+        cy.intercept('GET', 'http://omdbapi.com/?apikey=416ed51a&s=*', {fixture: 'mockListOfMovies'});
+        cy.get('#searchText').type('Remember Me').should('have.value', 'Remember Me');
+        cy.get('form').submit();
+        
+        cy.get('#movie-container > div').should('have.class', 'movie');
+        cy.get('#movie-container > div').should('have.length', 5);
+        cy.get('.movie > img').should('have.length', 5);
+        cy.get('.movie > h3').should('have.length', 5);
+        cy.get('.movie > h3').should('contain', 'Remember Me');
+        cy.get('.movie > h3').should('contain', 'The Green Mile');
+        cy.get('.movie > h3').should('contain', 'The Departed');
+        cy.get('.movie > h3').should('contain', 'Anaconda');
+        cy.get('.movie > h3').should('contain', 'Shutter Island');
     });
 });
